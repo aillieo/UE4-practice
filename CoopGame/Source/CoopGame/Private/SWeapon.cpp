1ae3670bd5c33e1bb8d51e09bad668adc93a9426
aillieo <aillieo@163.com>
2021-10-16 22:36:07 +0800

    finish ep 66

--- CoopGame/Source/CoopGame/Private/SWeapon.cpp
+++ CoopGame/Source/CoopGame/Private/SWeapon.cpp
@@ -9,6 +9,14 @@
 #include "Particles/ParticleSystemComponent.h"
 #include "Components/SceneComponent.h"
 
+static int32 DebugWeaponDrawing = 0;
+FAutoConsoleVariableRef ACVRDebugWeaponDrawing(
+        TEXT("Coop.DebugWeapons"),
+        DebugWeaponDrawing,
+        TEXT("Draw debug lines for weapon"),
+        ECVF_Cheat
+        );
+
 // Sets default values
 ASWeapon::ASWeapon()
 {
@@ -66,7 +74,10 @@ void ASWeapon::Fire()
 			}
 		}
 
-		DrawDebugLine(GetWorld(), EyeLocation, TraceEnd, FColor::White, false, 1.0f, 0, 1.0f);
+		if (DebugWeaponDrawing > 0)
+        {
+            DrawDebugLine(GetWorld(), EyeLocation, TraceEnd, FColor::White, false, 1.0f, 0, 1.0f);
+        }
 
 		if (MuzzleEffect != nullptr)
 		{
